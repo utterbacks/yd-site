c0880a9c2e318b69ffbd2051b6695452ab6bd715
Stephen Utterback <utterbackstephen@gmail.com>
2021-06-25 14:24:51 -0500

    links to streaming added

--- components/HeadCarousel.js
+++ components/HeadCarousel.js
@@ -1,14 +1,16 @@
 export default function HeadCarousel(props) {
-	const desktopIMG = props.images.desktopImage.formats.large.url;
-	const mobileIMG = props.images.mobileImage.formats.large.url;
-	const isMobile = props.windowSize.width < 768;
+	// const desktopIMG = props.images.desktopImage.formats.large.url;
+	// const mobileIMG = props.images.mobileImage.formats.large.url;
+	// const isMobile = props.windowSize.width < 768;
 
 	return (
 		<section
 			className="head"
-			style={{
-				backgroundImage: isMobile ? `url(${mobileIMG})` : `url(${desktopIMG})`,
-			}}
+			style={
+				{
+					// backgroundImage: isMobile ? `url(${mobileIMG})` : `url(${desktopIMG})`,
+				}
+			}
 		></section>
 	);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,41 @@
+import Head from "next/head";
+
 import axios from "axios";
 import apiUrl from "../utils/apiUrl";
 
 import HeadCarousel from "../components/HeadCarousel";
 import About from "../components/About";
 import Shows from "../components/Shows";
+import Listen from "../components/Listen";
 
 export default function Home(props) {
-	console.log(props);
+	// console.log(props);
 
 	return (
 		<div className="wrapper">
+			<Head>
+				{/* This adds links to the head tag of the html. Specifically, this is the font-awesome CDN for social links. */}
+
+				<link
+					rel="stylesheet"
+					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+					integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+					crossorigin="anonymous"
+					referrerpolicy="no-referrer"
+				/>
+			</Head>
 			<HeadCarousel
-				windowSize={props.windowSize}
-				images={props.index.headerImage}
+			// windowSize={props.windowSize}
+			// images={props.index.headerImage}
 			/>
 
-			<About members={props.index.Members} />
+			<About
+			// members={props.index.Members}
+			/>
 
 			<Shows
-				heading={props.index.showsHeading}
-				shows={props.index.SingleShows}
+			// heading={props.index.showsHeading}
+			// shows={props.index.SingleShows}
 			/>
 
 			{/* 
@@ -37,18 +53,7 @@ export default function Home(props) {
 				</div>
 			</section> */}
 
-			<section className="music">
-				<h1>Take a Listen</h1>
-				{/* eslint-disable-next-line */}
-				<iframe
-					src="https://open.spotify.com/embed/artist/0v26XwuwtnWuVkTVjbBsvv"
-					width="300"
-					height="200"
-					frameBorder="0"
-					allowtransparency="true"
-					allow="encrypted-media"
-				></iframe>
-			</section>
+			<Listen />
 
 			<section className="merch">
 				<h1>Merch</h1>
@@ -58,12 +63,12 @@ export default function Home(props) {
 	);
 }
 
-Home.getInitialProps = async (ctx) => {
-	try {
-		const res = await axios.get(apiUrl("yd-backend"));
-		const index = res.data;
-		return { index };
-	} catch (error) {
-		return { error };
-	}
-};
+// Home.getInitialProps = async (ctx) => {
+// 	try {
+// 		const res = await axios.get(apiUrl("yd-backend"));
+// 		const index = res.data;
+// 		return { index };
+// 	} catch (error) {
+// 		return { error };
+// 	}
+// };
